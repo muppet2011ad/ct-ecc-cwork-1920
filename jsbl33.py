cda40aea0ef143cbdd8a039722a140bae4093d6d
James Townsend <j.a.townsend2000@gmail.com>
2020-03-05 11:47:20 +0000

    Decimal to vector function completed and hamming code imported

---
+++ jsbl33.py
@@ -0,0 +1,45 @@
+def decimalToVector(i, l):
+    binary = bin(i)
+    binarray = [int(x) for x in binary[2:]]
+    padding = [0] * (l - len(binarray))
+    return padding + binarray
+
+
+# function HammingG
+# input: a number r
+# output: G, the generator matrix of the (2^r-1,2^r-r-1) Hamming code
+def hammingGeneratorMatrix(r):
+    n = 2 ** r - 1
+
+    # construct permutation pi
+    pi = []
+    for i in range(r):
+        pi.append(2 ** (r - i - 1))
+    for j in range(1, r):
+        for k in range(2 ** j + 1, 2 ** (j + 1)):
+            pi.append(k)
+
+    # construct rho = pi^(-1)
+    rho = []
+    for i in range(n):
+        rho.append(pi.index(i + 1))
+
+    # construct H'
+    H = []
+    for i in range(r, n):
+        H.append(decimalToVector(pi[i], r))
+
+    # construct G'
+    GG = [list(i) for i in zip(*H)]
+    for i in range(n - r):
+        GG.append(decimalToVector(2 ** (n - r - i - 1), n - r))
+
+    # apply rho to get Gtranpose
+    G = []
+    for i in range(n):
+        G.append(GG[rho[i]])
+
+    # transpose
+    G = [list(i) for i in zip(*G)]
+
+    return G
